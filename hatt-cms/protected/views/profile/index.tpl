c1ee69d14cf77aa02160a3888ffcd5443a77fe4f
Dmitry Larchikov <dimka3210@gmail.com>
2013-12-16 22:25:02 +0400

    profile to table bootstrap

--- hatt-cms/protected/views/profile/index.tpl
+++ hatt-cms/protected/views/profile/index.tpl
@@ -1,97 +1,31 @@
-<div class="profile-view">
-    <div class="row">
-        <div class="span12">
-            <div class="fieldset">
-                Профиль пользователя {$user_info.login}
-            </div>
-        </div>
-    </div>
-
-    <!-- id -->
-    <div class="row">
-        <div class="span2"></div>
-        <div class="span6 m10 br4">
-            <div class="row">
-                <div class="span3">Ваш ID в системе:</div>
-                <div class="span3">{$user_info.id}</div>
-            </div>
-        </div>
-        <div class="span4"></div>
-    </div>
-    <!-- id/END -->
-
-    <!-- email -->
-    <div class="row">
-        <div class="span2"></div>
-        <div class="span6 m10 br4">
-            <div class="row">
-                <div class="span3">
-                    Email:
-                </div>
-                <div class="span3">
-                    {$user_info.email}
-                </div>
-            </div>
-        </div>
-        <div class="span4"></div>
-    </div>
-    <!-- email/END -->
-
-    <!-- reg_date -->
-    <div class="row">
-        <div class="span2"></div>
-        <div class="span6 m10 br4">
-            <div class="row">
-                <div class="span3">
-                    Дата регистрации:
-                </div>
-                <div class="span3">
-                    {$user_info.reg_date}
-                </div>
-            </div>
-        </div>
-        <div class="span4"></div>
-    </div>
-    <!-- reg_date/END -->
-
-    <!-- avatar -->
-    <div class="row">
-        <div class="span2"></div>
-        <div class="span6 m10 br4">
-            <div class="row">
-                <div class="span3">
-                    Аватар:
-                </div>
-                <div class="span3">
-                    {*{$user_info.avatar}*}
-                    <img src="http://dummyimage.com/100/000/fff&text=%D0%90%D0%B2%D0%B0%D1%82%D0%B0%D1%80%D0%BA%D0%B0"/>
-                </div>
-            </div>
-        </div>
-        <div class="span4"></div>
-    </div>
-    <!-- avatar/END -->
-
-    <!-- gender -->
-    <div class="row">
-        <div class="span2"></div>
-        <div class="span6 m10 br4">
-            <div class="row">
-                <div class="span3">
-                    Пол:
-                </div>
-                <div class="span3">
-                    {if $user_info.gender == 0}
-                        Не указано
-                    {elseif $user_info.gender == 1}
-                        Мужской
-                    {else}
-                        Женский
-                    {/if}
-                </div>
-            </div>
-        </div>
-        <div class="span4"></div>
-    </div>
-    <!-- gender/END -->
-</div>
+<table class="table table-striped">
+    <tr>
+        <td>Ваш ID в системе:</td>
+        <td>{$user_info.id}</td>
+    </tr>
+    <tr>
+        <td>Email:</td>
+        <td>{$user_info.email}</td>
+    </tr>
+    <tr>
+        <td>Дата регистрации:</td>
+        <td>{$user_info.reg_date}</td>
+    </tr>
+    <tr>
+        <td>Аватар:</td>
+        <td>{*{$user_info.avatar}*}
+            <img src="http://dummyimage.com/100/000/fff&text=%D0%90%D0%B2%D0%B0%D1%82%D0%B0%D1%80%D0%BA%D0%B0"/></td>
+    </tr>
+    <tr>
+        <td>Пол:</td>
+        <td>
+            {if $user_info.gender == 0}
+                Не указано
+            {elseif $user_info.gender == 1}
+                Мужской
+            {else}
+                Женский
+            {/if}
+        </td>
+    </tr>
+</table>
--- hatt-cms/sql/dump.sql
+++ hatt-cms/sql/dump.sql
@@ -17,20 +17,40 @@ USE `hatt`;
 
 -- Дамп структуры для таблица hatt.categories
 CREATE TABLE IF NOT EXISTS `categories` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `categories_group_id` int(11) unsigned DEFAULT NULL,
   `name` varchar(128) NOT NULL DEFAULT '',
   `is_main` tinyint(1) NOT NULL DEFAULT '0' COMMENT 'Показывать на главной',
-  `order` int(11) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`)
+  `sort_index` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `FK_categories_categories_group` (`categories_group_id`),
+  CONSTRAINT `FK_categories_categories_group` FOREIGN KEY (`categories_group_id`) REFERENCES `categories_group` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COMMENT='Категории';
 
--- Дамп данных таблицы hatt.categories: ~0 rows (приблизительно)
+-- Дамп данных таблицы hatt.categories: ~3 rows (приблизительно)
 /*!40000 ALTER TABLE `categories` DISABLE KEYS */;
-INSERT INTO `categories` (`id`, `name`, `is_main`, `order`) VALUES
-	(1, 'Первая категория', 1, 0),
-	(2, 'Вторая категория', 1, 1),
-	(3, 'Категория с новостями', 0, 0);
+INSERT INTO `categories` (`id`, `categories_group_id`, `name`, `is_main`, `sort_index`) VALUES
+	(1, 2, 'Первая категория', 1, 0),
+	(2, 2, 'Вторая категория', 1, 1),
+	(3, 3, 'Категория с новостями', 0, 0);
 /*!40000 ALTER TABLE `categories` ENABLE KEYS */;
+
+
+-- Дамп структуры для таблица hatt.categories_group
+CREATE TABLE IF NOT EXISTS `categories_group` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(128) NOT NULL,
+  `sort_index` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+-- Дамп данных таблицы hatt.categories_group: ~1 rows (приблизительно)
+/*!40000 ALTER TABLE `categories_group` DISABLE KEYS */;
+INSERT INTO `categories_group` (`id`, `name`, `sort_index`) VALUES
+	(1, 'Моя первая группа', 0),
+	(2, 'Вторая группа', 1),
+	(3, 'Третья Группа', -1);
+/*!40000 ALTER TABLE `categories_group` ENABLE KEYS */;
 
 
 -- Дамп структуры для таблица hatt.news
@@ -48,6 +68,35 @@ CREATE TABLE IF NOT EXISTS `news` (
 -- Дамп данных таблицы hatt.news: ~0 rows (приблизительно)
 /*!40000 ALTER TABLE `news` DISABLE KEYS */;
 /*!40000 ALTER TABLE `news` ENABLE KEYS */;
+
+
+-- Дамп структуры для таблица hatt.topics
+CREATE TABLE IF NOT EXISTS `topics` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `users_id` int(11) unsigned DEFAULT NULL,
+  `categories_id` int(11) unsigned DEFAULT NULL,
+  `name` varchar(255) NOT NULL DEFAULT '',
+  `text` text NOT NULL,
+  `created_date` datetime DEFAULT NULL,
+  `modify_date` datetime DEFAULT NULL,
+  `status` tinyint(1) NOT NULL DEFAULT '1',
+  `mod_comment` varchar(128) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_topics_users` (`users_id`),
+  KEY `FK_topics_categories` (`categories_id`),
+  CONSTRAINT `FK_topics_categories` FOREIGN KEY (`categories_id`) REFERENCES `categories` (`id`) ON DELETE SET NULL ON UPDATE SET NULL,
+  CONSTRAINT `FK_topics_users` FOREIGN KEY (`users_id`) REFERENCES `users` (`id`) ON DELETE SET NULL ON UPDATE SET NULL
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+
+-- Дамп данных таблицы hatt.topics: ~5 rows (приблизительно)
+/*!40000 ALTER TABLE `topics` DISABLE KEYS */;
+INSERT INTO `topics` (`id`, `users_id`, `categories_id`, `name`, `text`, `created_date`, `modify_date`, `status`, `mod_comment`) VALUES
+	(1, 1, 1, 'sdfdsf', 'dczdvvdv', NULL, NULL, 1, ''),
+	(2, 1, 2, 'sdfdsf', 'dczdvvdv', NULL, NULL, 1, ''),
+	(3, 1, 3, 'sdfdsf', 'dczdvvdv', NULL, NULL, 1, ''),
+	(4, 1, 1, 'sdfdsf', 'dczdvvdv', NULL, NULL, 1, ''),
+	(5, 1, 3, 'sdfdsf', 'dczdvvdv', NULL, NULL, 1, '');
+/*!40000 ALTER TABLE `topics` ENABLE KEYS */;
 
 
 -- Дамп структуры для таблица hatt.users
@@ -72,7 +121,7 @@ CREATE TABLE IF NOT EXISTS `users` (
 -- Дамп данных таблицы hatt.users: ~3 rows (приблизительно)
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
 INSERT INTO `users` (`id`, `auth_token`, `login`, `email`, `pass`, `reg_date`, `level`, `reg_ip`, `avatar`, `gender`, `birthday`) VALUES
-	(1, 'adb41b6dd62ff97b50741762d2cce98e', 'dimka3210', 'dimka3210@gmail.com', '202cb962ac59075b964b07152d234b70', '2013-12-05 23:47:19', 1, 0, '', '1', '2013-12-05 23:47:28'),
+	(1, '7d208d3a61a55874cc309a7ca5ab4eab', 'dimka3210', 'dimka3210@gmail.com', '202cb962ac59075b964b07152d234b70', '2013-12-05 23:47:19', 1, 0, '', '2', '2013-12-05 23:47:28'),
 	(2, NULL, '123', '', '', '0000-00-00 00:00:00', 0, 0, NULL, '0', '0000-00-00 00:00:00'),
 	(4, 'cdec3d1ee82ddb07f7de11005d5f87d5', 'dimka32101', 'dimka3210@gmail.com1', '202cb962ac59075b964b07152d234b70', '2013-12-05 23:47:19', 1, 0, '', '1', '2013-12-05 23:47:28');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
